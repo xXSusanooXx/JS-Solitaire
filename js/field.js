5b553bc3bb483f8b29198bb3fec6263b700298c8
Susano <Myth_w3x@mail.ru>
2016-04-04 18:27:35 +0300

    first attempt to drag and drop

--- js/field.js
+++ js/field.js
@@ -8,45 +8,66 @@ function onClick ( event ) {
 				clickOnSafeCell(cell);
                 cell.disabled='disabled';
 			}
-		}
-	}
-
-var field=document.getElementById('field');
-field.onmousedown=mouseDown;
-
-
-function mouseDown ( e ) {
-/*
-  var coords = getCoords(ball);
-  var shiftX = e.pageX - coords.left;
-  var shiftY = e.pageY - coords.top;
-*/
-  ball.style.position = 'absolute';
-  document.body.appendChild(ball);
-  moveAt(e);
-
-  ball.style.zIndex = 1000; // над другими элементами
-
-  function moveAt(e) {
-    ball.style.left = e.pageX - shiftX + 'px';
-    ball.style.top = e.pageY - shiftY + 'px';
-  }
-
-  document.onmousemove = function(e) {
-    moveAt(e);
-  };
-
-  ball.onmouseup = function() {
-    document.onmousemove = null;
-    ball.onmouseup = null;
-  };
-
+        }
 }
 
+var field=document.getElementById('field');
+field.onmousedown=onMouseDown;
+
+function onMouseDown ( event ) {
+    function isCard(element) {
+        if (element.classList.contains('card')) {
+            return true;
+        }
+        return false;
+    }
+
+    function getCoords(element) {
+        var box = element.getBoundingClientRect();
+
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset
+        };
+
+    }
+
+    if (isCard(event.target)) {
+        var card = event.target;
+        /*card.ondragstart = function() {
+            return false;
+        };*/
+        var coords = getCoords(card);
+        var shiftX = event.pageX - coords.left;
+        var shiftY = event.pageY - coords.top;
+        card.style.position = 'absolute';
+        //moveAt(event);
+
+        //document.body.appendChild(card);
+        //card.style.zIndex = 1000; // над другими элементами
+
+        function moveAt(event) {
+            card.style.left = event.pageX - shiftX + 'px';
+            card.style.top = event.pageY - shiftY + 'px';
+        }
+        document.onmousemove = function (event) {
+            moveAt(event);
+        };
+
+        card.onmouseup = function () {
+            document.onmousemove = null;
+            card.onmouseup = null;
+           // card.style.position="static";
+           // card.style.zIndex=0;
+        };
+
+    }
+}
+/*
 ball.ondragstart = function() {
   return false;
 };
 
 	var ball = document.getElementById('ball');
 
-ball.onmousedown = function(e)
+ball.onmousedown = function(event)*/
--- js/loadThePilesOfCards.js
+++ js/loadThePilesOfCards.js
@@ -7,30 +7,59 @@ function shuffle(a) {
 		a[j] = x;
 	}
 }
-
+document.body.onselectstart= function() {return false};
 shuffle(arrayOfCards);
 var hidenCardsDeck = arrayOfCards.splice(0,24);
 var openCardsDeck=[];
 var piles=[];
 var NUMBERoFpILES = 7;
-for(var i=0;i<NUMBERoFpILES;i++)
+
+piles[0]=[1];
+piles[0][0]=arrayOfCards.splice(0,1)[0];
+var nowPileDiv=document.getElementById(1);
+nowPileDiv.classList.remove('shadow');
+nowPileDiv.classList.add('card');
+setBackground(nowPileDiv,piles[0][0].path);
+
+for(var i=1;i<NUMBERoFpILES;i++)
 {
-	piles[i]=[i];
-	for(var j=0;j<i+1;j++)
+	piles[i]=[i+1];
+    piles[i][0]=arrayOfCards.splice(0,1)[0];
+    var nowPileDiv=document.getElementById(i+1);
+    nowPileDiv.classList.remove('shadow');
+    nowPileDiv.classList.add('shirt');
+
+    for(var j=1;j<i;j++)
 	{
-		piles[i][j]=arrayOfCards.splice(0,1);
-	}
-	console.log(piles[i]);
+        piles[i][j]=arrayOfCards.splice(0,1)[0];
+        var cardToAddDiv = document.createElement('div');
+        cardToAddDiv.classList.add('shirt');
+        nowPileDiv.appendChild(cardToAddDiv);
+        nowPileDiv=cardToAddDiv;
+    }
+    piles[i][i]=arrayOfCards.splice(0,1)[0];
+    var cardToAddDiv = document.createElement('div');
+    cardToAddDiv.classList.add('card');
+    setBackground(cardToAddDiv,piles[i][i].path);
+    nowPileDiv.appendChild(cardToAddDiv);
 }
 
 var hideDiv=document.getElementsByClassName('hiden-cards-deck')[0];
-hideDiv.onclick=onDeckClick;
+var field=document.getElementById('field');
+field.onclick=function (event) {
+    if(event.target.classList.contains('hiden-cards-deck')){
+        onDeckClick();
+    }
+}
+
+
+
+//hideDiv.onclick=onDeckClick;
 function onDeckClick(){
 	if(hidenCardsDeck.length!==0)
 	{
 		openCardsDeck[openCardsDeck.length]=hidenCardsDeck.splice(0,1)[0];
 		var openDiv=document.getElementsByClassName('open-cards-deck')[0];
-		console.log(openCardsDeck);
 		setBackground(openDiv,openCardsDeck[openCardsDeck.length-1].path);
 	}
 	else
@@ -43,3 +72,34 @@ function onDeckClick(){
 function setBackground(element,path){
 	element.style.background="url("+path+")";
 }
+/*
+var el = document.getElementById(1);
+var child = document.createElement('div');
+child.classList.add('card');
+setBackground(child,arrayOfCards[0].path);
+el.appendChild(child);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
